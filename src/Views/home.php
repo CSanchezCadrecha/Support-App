ee3ec77a428ee49030f9660f19ffa83132b102db
Marina González Suárez <yelose85@gmail.com>
2022-07-15 12:26:37 +0200

    nothing changed

--- src/Views/home.php
+++ src/Views/home.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Base MVC with PHP</title>
+    <script src="https://kit.fontawesome.com/4029e392d6.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <h1>Fem Desk</h1>
 
     <?php 
-        var_dump($data);
+       var_dump($data);
 
         foreach ($data as $key => $value) {
             $html = <<<HTML
@@ -23,6 +24,7 @@
         }
 
         echo "home sweet home. Hello {$data['name']}." 
+
     ?>
 
 </body>

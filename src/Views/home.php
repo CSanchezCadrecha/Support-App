7adfb797fa3125c368c0a942160d6adf66d410f7
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-07-18 11:39:23 +0200

    added components folder and edit link

--- src/Views/home.php
+++ src/Views/home.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Base MVC with PHP</title>
+    <title>Home</title>
 </head>
 <body>
-    
+    <h1>Home View</h1>
+    <a href="?action=edit">Editar</a>
 
     <?php 
-        var_dump($data);
+       // var_dump($data);
 
-        foreach ($data as $key => $value) {
+       /* foreach ($data as $key => $value) {
             $html = <<<HTML
                 <div>
                     $key . ':' . $value
@@ -22,7 +23,7 @@
             echo $html;
         }
 
-        echo "home sweet home. Hello {$data['name']}." 
+        echo "home sweet home. Hello {$data['name']}." */
     ?>
 
 </body>

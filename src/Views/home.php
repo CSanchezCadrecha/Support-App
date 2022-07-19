8459a27ef4afb9f1cca88426989657a369887c75
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-07-19 11:24:31 +0200

    added style in edit and new files

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="en">
-
-=======
 <html lang="es">
->>>>>>> develop
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="src\css\style.css">
     <title>Home</title>
 </head>
 
 <body>
-    <h1>Home View</h1>
     <header>
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <img id="logo-img" class="shadow bg-white rounded" src="src/assets/logo.png" alt="imagen logo">
+                <img id="logo-img" src="src/assets/FemDesk-logo.png" alt="imagen logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="?action=home">Home</a>
                         <a class="nav-link" href="?action=edit">Editar</a>
                         <a class="nav-link" href="?action=new">Nueva Solicitud</a>
                     </div>
--- src/Views/new.php
+++ src/Views/new.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="src\css\style.css">
     <title>New</title>
 </head>
 <body>
-    <h1>New View</h1>
+<header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <img id="logo-img" src="src/assets/FemDesk-logo.png" alt="imagen logo">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link" aria-current="page" href="?action=home">Home</a>
+                        <a class="nav-link" href="?action=edit">Editar</a>
+                        <a class="nav-link active" href="?action=new">Nueva Solicitud</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h3>Formulario Nueva Solicitud</h3>
+        <div id="list"></div>
+    </main>
+    <footer>
+        <div class="d-flex justify-content-center">
+            <h6>©2022 FEM Desk</h6>
+        </div>
+    </footer>
     
 </body>
 </html>
